fccc12e120218a36e82e7a92cf4eaa72beb11516
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-05-14 15:05:45 -0400

    Pass session cookie options from environment

--- api/app.py
+++ api/app.py
@@ -11,6 +11,10 @@ app = flask.Flask(__name__)
 app.config['DEBUG'] = os.getenv('DEBUG', False)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SESSION_COOKIE_NAME'] = 'gatewaysession'
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_DOMAIN'] = os.getenv('FLASK_SESSION_DOMAIN', None)
+app.config['SESSION_COOKIE_SECURE'] = os.getenv('FLASK_SESSION_SECURE', False)
 app.secret_key = os.getenv('FLASK_SESSION_SECRET_KEY')
 
 db.init_app(app)
@@ -23,8 +27,8 @@ login_manager.init_app(app)
 def load_user(user_id):
     return db.session.query(User).filter(User.id == user_id).first()
 
-## TODO: login
 ## TODO: RBAC
+## TODO: CSRF support using header?
 
 with app.app_context():
     db.create_all() ## TODO: move to separate migration script? like https://realpython.com/blog/python/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
