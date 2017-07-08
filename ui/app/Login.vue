fa0e114e6d39c1882fe18bf7ac931028f2d45300
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-07-08 16:01:38 -0400

    Fix login page layout

--- ui/app/Login.vue
+++ ui/app/Login.vue
@@ -2,37 +2,32 @@
   <v-app>
     <main>
       <v-container fluid>
-        <h4>API Gateway</h4>
-        <v-layout>
-          <v-flex xs4 offset-xs4>
-            <v-card>
-              <v-container fluid grid-list-lg>
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-text-field
-                      name="username"
-                      label="Username"
-                      id="username"></v-text-field>
-                  </v-flex>
-                </v-layout>
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-text-field
-                      type="password"
-                      name="password"
-                      label="Password"
-                      id="password"></v-text-field>
-                  </v-flex>
-                </v-layout>
-                <v-layout row>
-                  <v-flex xs2 offset-xs8>
-                    <v-btn light primary>Login</v-btn>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
-          </v-flex>
-        </v-layout>
+        <h4 class="login-header">API Gateway</h4>
+        <v-card class="login-card">
+          <v-container fluid grid-list-lg>
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field
+                  name="username"
+                  label="Username"
+                  id="username"></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field
+                  type="password"
+                  name="password"
+                  label="Password"
+                  id="password"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn primary>Login</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-container>
     </main>
   </v-app>
@@ -50,7 +45,17 @@
 <style lang="stylus">
   @import '../node_modules/vuetify/src/stylus/main'
 
-  h4 {
-    text-align: center
+  .application--light {
+    background: #ffffff;
+  }
+
+  .login-header {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  .login-card {
+    width: 320px;
+    margin: 0 auto;
   }
 </style>
